9f2311ed9f4e0c4e42629eaedb6fc07333eec178
thomascswalker <thomascswalker@gmail.com>
2022-04-23 21:44:44 -0400

    sort of got the installer working

--- installer/installer.py
+++ installer/installer.py
@@ -7,25 +7,20 @@ from collections import namedtuple
 
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtCore import QFile
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox
 
+PACKAGE = "better-max-tools-thomascswalker"
 GITHUB = r"https://github.com/thomascswalker/better-max-tools"
 APPLICATION_PLUGINS_PATH = r"C:\ProgramData\Autodesk\ApplicationPlugins"
 AUTODESK_PATH = r"C:\Program Files\Autodesk"
 SITE_PACKAGES = os.path.join(os.getenv("appdata"), r"Python\Python37\site-packages")
 
 MaxInstall = namedtuple(
-    'MaxInstall',
-    [
-        'directory',
-        'version',
-        'pythonExecutable',
-        'sitePackages'
-    ]
+    "MaxInstall", ["directory", "version", "pythonExecutable", "sitePackages"]
 )
 
-def get_max_installs() -> List[Tuple[str]]:
 
+def get_max_installs() -> List[Tuple[str]]:
     dirs = glob(AUTODESK_PATH + r"\3ds Max*")
     result = []
 
@@ -35,34 +30,76 @@ def get_max_installs() -> List[Tuple[str]]:
 
             if version == 2022:
                 pythonExecutable = os.path.join(dir, "Python37\\python.exe")
-                sitePackages = os.path.join(os.getenv("appdata"), "Python\\Python37\\site-packages")
+                sitePackages = os.path.join(
+                    os.getenv("appdata"), "Python\\Python37\\site-packages"
+                )
             elif version == 2023:
                 pythonExecutable = os.path.join(dir, "Python39\\python.exe")
-                sitePackages = os.path.join(os.getenv("appdata"), "Python\\Python39\\site-packages")
+                sitePackages = os.path.join(
+                    os.getenv("appdata"), "Python\\Python39\\site-packages"
+                )
             else:
-                pythonExecutable = os.path.join(dir, "Python\\python.exe")
-                sitePackages = os.path.join(os.getenv("appdata"), "Python\\Python37\\site-packages")
+                pythonExecutable = os.path.join(dir, "Python37\\python.exe")
+                sitePackages = os.path.join(
+                    os.getenv("appdata"), "Python\\Python37\\site-packages"
+                )
 
-            MaxInstall(dir, version, pythonExecutable, sitePackages)
-            result.append(MaxInstall)
+            install = MaxInstall(dir, version, pythonExecutable, sitePackages)
+            result.append(install)
 
     return result
 
 
-def is_package_installed() -> bool:
+def is_package_installed(install: MaxInstall) -> bool:
+    args = [install.pythonExecutable, "-m", "pip", "list"]
+    result = subprocess.check_output(args)
+    for line in result.decode().split("\n"):
+        if "better-max-tools" in line:
+            return True
+
+    return False
+
+
+def install_package(install: MaxInstall) -> bool:
+    interpreter = install.pythonExecutable
+    if not os.path.exists(interpreter):
+        raise FileNotFoundError("Python interpreter not found.")
+
+    subprocess.Popen(f"{interpreter} -m ensurepip")
+
+    args = [interpreter, "-m", "pip", "install", PACKAGE]
+    output = subprocess.check_output(args)
+
+    QMessageBox.information(None, "Better Max Tools", "Installed!")
+
+    return True
+
+
+def uninstall_package(install: MaxInstall) -> bool:
+    interpreter = install.pythonExecutable
+    if not os.path.exists(interpreter):
+        raise FileNotFoundError("Python interpreter not found.")
+
+    subprocess.Popen(f"{interpreter} -m ensurepip")
+
+    args = [interpreter, "-m", "pip", "uninstall", "-y", PACKAGE]
+    output = subprocess.check_output(args)
+
+    QMessageBox.information(None, "Better Max Tools", "Uninstalled!")
+
     return True
 
 
 class InstallerWindow(QMainWindow):
     _installations: List[MaxInstall] = get_max_installs()
-    _currentVersion: str
-    _currentMaxPath: str
+    _currentInstall: MaxInstall = _installations[0]
 
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("Better Max Tools Installer")
         self.setupUi()
         self.setupConnections()
+        self.updateInstallButtonState()
         self.setupStyle()
 
     def setupUi(self) -> None:
@@ -77,28 +114,28 @@ class InstallerWindow(QMainWindow):
 
         self.setCentralWidget(self._centralWidget)
         for installation in self._installations:
-            print(installation.version)
-            print(installation.directory)
             item = (f"3ds Max {installation.version}", installation.directory)
             self._centralWidget.maxVersionList.addItem(*item)
-        self._centralWidget.maxVersionList.addItem('test')
+
         self.setInstallPath()
 
     def setupConnections(self):
         self._centralWidget.maxVersionExplore.clicked.connect(self.exploreMaxVersion)
+        self._centralWidget.install.clicked.connect(self.install)
+        self._centralWidget.uninstall.clicked.connect(self.uninstall)
 
-        isInstalled = is_package_installed()
+    def updateInstallButtonState(self):
+        isInstalled = is_package_installed(self._currentInstall)
         if not isInstalled:
+            self._centralWidget.install.setEnabled(True)
             self._centralWidget.uninstall.setEnabled(False)
-            self._centralWidget.install.clicked.connect(self.install)
         else:
             self._centralWidget.install.setEnabled(False)
             self._centralWidget.uninstall.setEnabled(True)
-            self._centralWidget.uninstall.clicked.connect(self.uninstall)
 
     def setupStyle(self):
         qssFileName = f"{os.path.dirname(__file__)}\\windows.qss"
-        with open(qssFileName, 'r') as file:
+        with open(qssFileName, "r") as file:
             qss = "".join(file.readlines())
         self.setStyleSheet(qss)
 
@@ -111,23 +148,15 @@ class InstallerWindow(QMainWindow):
             subprocess.Popen(f'explorer "{path}"')
 
     def install(self):
-        maxPath = self._centralWidget.maxVersionList.currentData()
-        version = "37"
-        interpreter = os.path.join(maxPath, f"Python{version}\\python.exe")
-        if not os.path.exists(interpreter):
-            raise FileNotFoundError("Python interpreter not found.")
-
-        subprocess.Popen(f"{interpreter} -m ensurepip")
-
-        packages = ["better-max-tools-thomascswalker"]
-        for package in packages:
-            subprocess.Popen(f"{interpreter} -m pip install {package}")
+        install_package(self._currentInstall)
+        self.updateInstallButtonState()
 
     def uninstall(self):
-        pass
+        uninstall_package(self._currentInstall)
+        self.updateInstallButtonState()
 
 
-def run():
+def main():
     app = QApplication(sys.argv)
 
     dialog = InstallerWindow()
@@ -137,4 +166,4 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
+    main()
